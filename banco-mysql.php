796fbbf132a1655a7753bf02c49baa552760b9c8
Raniel Mendonça <ranieel@outlook.com>
2017-06-18 23:02:22 -0300

    Criação e população da tabela usuarios feita em PHP-OO utilizando MySQL

--- banco-mysql.php
+++ banco-mysql.php
@@ -1,1 +1,45 @@
-//escreva o código aqui
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "banco";
+$dbname = "dist";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+} 
+
+// sql to create table
+$usuarios = "CREATE TABLE usuarios (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+primeironome VARCHAR(30) NOT NULL,
+segundonome VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+tipopermissao INT(1) NOT NULL,
+cargo VARCHAR(30),
+phone VARCHAR(15),
+nomeusuario VARCHAR(30),
+senha VARCHAR(30) NOT NULL,
+reg_date TIMESTAMP,
+CONSTRAINT usuario UNIQUE (id,email,nomeusuario)
+)";
+
+if ($conn->query($usuarios) === TRUE) {
+    echo "A tabela usuários foi criada com sucesso.";
+} else {
+    echo "Erro ao criar a tabela usuários: " . $conn->error;
+}
+
+$insereusuarios = "INSERT INTO usuarios (primeironome, segundonome, email, tipopermissao, cargo, phone, nomeusuario, senha)
+VALUES ('Raniel', 'Mendonça', 'ranieel@outlook.com', 0, 'administrador', '+55034992322116', 'raniel','102030')";
+
+if ($conn->query($insereusuarios) === TRUE) {
+    echo "Nova gravação persistida com sucesso.";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
