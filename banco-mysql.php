8da0eae1799bdbc45a6764529d0e52c6056d458f
Raniel Mendonça <ranieel@outlook.com>
2017-06-21 18:19:21 -0300

    Criacao e populacao das tabelas usuarios, setores e produtos

--- banco-mysql.php
+++ banco-mysql.php
@@ -26,15 +26,57 @@ reg_date TIMESTAMP
 )";
 
 if ($conn->query($usuarios) === TRUE) {
-    echo "A tabela usuários foi criada com sucesso.";
+    echo "A tabela usuários foi criada com sucesso.\n";
 } else {
     echo "Erro ao criar a tabela usuários: " . $conn->error;
 }
 
-$insereusuarios = "INSERT INTO usuarios (primeironome, segundonome, email, tipopermissao, cargo, phone, nomeusuario, senha)
-VALUES ('Raniel', 'Mendonça', 'ranieel@outlook.com', 0, 'administrador', '+55034992322116', 'raniel','102030')";
+$setores = "CREATE TABLE setores (
+(ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nomesetor VARCHAR(30) NOT NULL
+reg_date TIMESTAMP
+)";
 
-if ($conn->query($insereusuarios) === TRUE) {
+if ($conn->query($setores) === TRUE) {
+    echo "A tabela setores foi criada com sucesso.\n";
+} else {
+    echo "Erro ao criar a tabela setores " . $conn->error;
+}
+
+$insereusetores = "INSERT INTO setores (nomesetor)
+VALUES ('Principal')";
+
+if ($conn->query($insereusetores) === TRUE) {
+    echo "Nova gravação persistida com sucesso.";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$produtos = "CREATE TABLE produtos (
+ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nomeproduto VARCHAR(60) NOT NULL,
+qtdadeestoque INT,
+alertaexpiracao INT,
+dataexpiracao DATE,
+perecivel BOOLEAN,
+fabricante VARCHAR(30),
+estoqueminimo INT,
+setor INT(6) UNSIGNED,
+tipo VARCHAR(30),
+FOREIGN KEY (setor) REFERENCES setores (ID),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($produtos) === TRUE) {
+    echo "A tabela produtos foi criada com sucesso.";
+} else {
+    echo "Erro ao criar a tabela produtos: " . $conn->error;
+}
+
+$insereprodutos = "INSERT INTO produtos (nomeproduto, qtdadeestoque, alertaexpiracao, dataexpiracao, perecivel, fabricante, estoqueminimo, setor, tipo)
+VALUES ('Caneta', 5, 7, 0, 'bic', 30, 1, 'canetas')";
+
+if ($conn->query($insereprodutos) === TRUE) {
     echo "Nova gravação persistida com sucesso.";
 } else {
     echo "Erro: " . $sql . "<br>" . $conn->error;
