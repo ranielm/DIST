03d135a137fafaf45c7d601dd998d96e609a6fe0
amandasouzac <amandasouzac2009@hotmail.com>
2017-07-23 19:02:50 -0300

    Update UsuarioPessoa.php

--- classes/UsuarioPessoa.php
+++ classes/UsuarioPessoa.php
@@ -1,8 +1,38 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class UsuarioPessoa extends Usuario{
+    private $idade;             //integer
+    private $profissao;         //string
+    private $maxPostagens = 5;  //integer
+    private $lembrete;          //string
+    
+    public function __construct($int_idade, $string_profissao){
+        $this->idade =  $int_idade;
+        $this->profissao = $string_profissao;
+    }
+    
+    public function adicionarLembrete($string_lembrete, $string_data, $string_horario){
+        $this->lembrete = $string_lembrete . " em " . $string_data. " - " . $string_horario;
+    }
+    
+    public function setMaxPostagens($int_max){
+        $this->maxPostagens = $int_max;
+    }
+    
+    /****GETTERS****/
+    public function getIdade(){
+        return $this->idade;
+    }
+    
+    public function getProfissao(){
+        return $this->profissao;
+    }
+    
+    public function getMaxPostagens(){
+        return $this->maxPostagens;
+    }
+    
+    public function getLembrete(){
+        return $this->lembrete;
+    }
+}
