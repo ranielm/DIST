5d7814e6d2fb0d4d709f3c69bfbe3d7a83c97383
Raniel Mendonça <ranieel@outlook.com>
2017-07-27 07:17:23 -0300

    Criando memento

--- unique/views/posts/index.php
+++ unique/views/posts/index.php
@@ -4,7 +4,7 @@
   <p>
     <?php echo $post->title; ?>
     <a href='?controller=posts&action=show&id=<?php echo $post->id; ?>'>Ver conteúdo</a>
-    <a href='?controller=posts&action=excluirpost&title=<?php echo $post->title; ?>&content=<?php echo $post->content; ?>author=<?php echo $post->author; ?>'>Excluir</a>
+    <a href='?controller=posts&action=excluirpost&title=<?php echo $post->title; ?>&content=<?php echo $post->content; ?>&author=<?php echo $post->author; ?>'>Excluir</a>
     
   </p>
 <?php } ?>
