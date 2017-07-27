29e487d9baf727f6c802dc25a749d7f64d8aac20
Raniel Mendonça <ranieel@outlook.com>
2017-07-26 23:29:40 -0300

    Inserir dados postagem

--- unique/views/posts/inserir.php
+++ unique/views/posts/inserir.php
@@ -1,6 +1,6 @@
 <p>Digite as informações da postagem</p>
 
-<form action="controllers/posts_controller.php" method="post">
+<form action="http://localhost/unique/controllers/posts_controller.php" method="post">
     <div>
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" />
