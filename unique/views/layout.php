b743041fc912dd4b2e1a08c72c4db72bd1c3d0c3
Raniel Mendonça <ranieel@outlook.com>
2017-07-27 08:57:04 -0300

    Observer na posts

--- unique/views/layout.php
+++ unique/views/layout.php
@@ -7,7 +7,7 @@
       <a href='/unique'>Início</a>
       <a href='?controller=posts&action=index'>Notícias</a>
       <a href='?controller=posts&action=inserir'>Criar postagem</a>
-      <a href='/unique/models/class.splobserver.php'>Enviar notícias</a>
+      <a href='/unique/models/class.splobserver.php'>Observer</a>
     </header>
 
     <?php require_once('routes.php'); ?>
